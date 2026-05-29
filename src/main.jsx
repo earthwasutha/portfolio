f62a86dd4244cdb3462b6eeae44017e1635f7d3f
Wasutha Junpangam <wasutha.ju@gmail.com>
2026-05-29 12:05:47 +0700

    add visitor

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics'
+import { inject, Analytics } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
 
@@ -9,5 +9,6 @@ inject()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
